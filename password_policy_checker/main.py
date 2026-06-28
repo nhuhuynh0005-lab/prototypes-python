9ea0fd91f318d90f76a68d365f7acfc7bd917f47
Huynh Thi Nhu <nhuht@HuynhnoMacBook-Air.local>
2026-06-28 19:39:08 +0900

    da lam xong pj check pw xin moi thay check

--- password_policy_checker/main.py
+++ password_policy_checker/main.py
@@ -7,16 +7,20 @@ password = sys.argv[1]
 # else:
 #     print("❌ Has 8 or more characters")
 
+
 def is_long_enough(pw):
     return len(pw) >= 8
 
+
 # helper
+
 
 def format_bool(v):
     if v:
-        return '✅'
+        return "✅"
 
-    return '❌'
+    return "❌"
+
 
 print(f"{format_bool(is_long_enough(password))} Has 8 or more characters")
 
@@ -39,33 +43,62 @@ print(f"{format_bool(is_long_enough(password))} Has 8 or more characters")
 #     print("❌ Contains lower-case characters")
 
 
-def is_lower(pw):
+def has_lower(pw):
     for a in pw:
         if a.islower():
             return True
+    return False
 
-print(f"{format_bool(is_lower(password))} Contains lower_case characters")
+
+print(f"{format_bool(has_lower(password))} Contains lower_case characters")
 
 
-def is_upper(pw):
+def has_upper(pw):
     for x in pw:
         if x.isupper():
             return True
-        
-print(f"{format_bool(is_upper(password))} Contains upper_case characters")
+    return False
 
 
-def is_digit(pw):
+print(f"{format_bool(has_upper(password))} Contains upper_case characters")
+
+
+def has_digit(pw):
     for w in pw:
         if w.isdigit():
             return True
+    return False
 
-print(f"{format_bool(is_digit(password))} Contain numeric characters")
 
-def is_special(pw):
+print(f"{format_bool(has_digit(password))} Contain numeric characters")
+
+
+def has_special(pw):
     for p in pw:
-        if not  p.islower() and not p.isupper() and not p.isdigit():
+        # if not p.islower() and not p.isupper() and not p.isdigit():
+        if not p.isalnum():
             return True
-        
-print(f"{format_bool(is_special(password))} Contains special characters")
+    return False
 
+
+print(f"{format_bool(has_special(password))} Contains special characters")
+
+common_passwords = {
+    "123456",
+    "123456789",
+    "111111",
+    "password",
+    "qwerty",
+    "abc123",
+    "12345678",
+    "password1",
+    "1234567",
+    "123123",
+}
+
+
+def is_not_common_password(pw):
+    return pw not in common_passwords
+
+
+print(f"{format_bool(is_not_common_password(password))} Is not common")
