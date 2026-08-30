ae60767b6e046f7462264f3f12189f9e66aed9e0
Huynh Thi Nhu <nhuht@HuynhnoMacBook-Air.local>
2026-08-30 19:03:22 +0900

    đã hoàn thành game nhưng chưa sử dụng class

--- trivia-game/main.py
+++ trivia-game/main.py
@@ -1,5 +1,7 @@
 import json
 import os
+import random
+import sys
 
 # === DO NOT TOUCH ===
 
@@ -11,6 +13,53 @@ with open(QUESTION_JSON_PATH, 'r') as file:
 
 # === DO NOT TOUCH ===
 
-print(raw_questions[0])
-print(type(raw_questions[0]))
-print(len(raw_questions))
+# first_question = raw_questions[0]
+def format_question(question) -> str:
+    choice_with_label = []
+    for answer in question["choices"]:
+        choice_with_label.append(answer)
+    
+    corect_answer = choice_with_label[0]
+    random.shuffle(choice_with_label)
+
+    a_list = []
+    for ind, choice in enumerate(choice_with_label, start = 1 ):
+        a_list.append(f"{ind}. {choice}")
+        choices_str = "\n".join(a_list)
+        answer_list = "".join(question['prompt']) + "\n" + choices_str
+    return (
+        answer_list,
+        corect_answer,
+        choice_with_label
+        )
+    
+i = 0
+question_random = random.sample(raw_questions,10)
+for quiz in question_random:
+    ans_list,cor_answer,choice_with_label1 = format_question(quiz)
+    print(ans_list)
+     
+    try:
+        while True:
+            input_keyword = input(">")
+            if input_keyword.isdigit() and input_keyword < "5":
+                input_keyword_int = int(input_keyword)
+                answer = choice_with_label1[input_keyword_int-1]
+                if answer  ==  cor_answer :
+                    print("✔ correct")
+                    i += 1
+                else :
+                    print(f"✖ incorect.The correct answer is {choice_with_label1.index(cor_answer) + 1}")
+                break
+            else:
+                if input_keyword == "":
+                    continue
+                else:
+                    print(f"✖ incorect.The correct answer is {choice_with_label1.index(cor_answer) + 1}")
+                    break
+    except KeyboardInterrupt:
+    
+        sys.exit()
+        
+print(f"Your score is {i}/10")
+
